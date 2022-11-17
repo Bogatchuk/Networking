752adcec87dfe41aaf5213bf725edcd383c080d0
Bogatchuk Roman <bogatchukroma2013@gmail.com>
2022-11-17 16:13:50 +0300

    Download Image

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var getImageButton: UIButton!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
     }
 
 
+    @IBAction func getImagePressed(_ sender: UIButton) {
+        activityIndicator.startAnimating()
+        getImageButton.isEnabled = false
+        label.isHidden = true
+
+        guard let url = URL(string: "https://vjoy.cc/wp-content/uploads/2019/12/3dr-19.jpg") else { return }
+
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {return}
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                self.imageView.image = image
+            }
+        }.resume()
+        
+    }
 }
 
